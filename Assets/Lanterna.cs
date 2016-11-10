5d11e4449c0449c06a5d517f8fb299f729856e28
Francisco Calaça Xavier <chicocx@gmail.com>
2016-11-10 13:10:16 -0200

    First commit

---
+++ Assets/Lanterna.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lanterna : MonoBehaviour {
+
+    private AndroidJavaClass ajc;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Androidtion is to be called");
+
+
+        ajc = new AndroidJavaClass("br.eti.francisco.lightcontrolmodule.LightControlUtil");
+        ajc.CallStatic("lightOn");
+        Debug.Log("lightOn is called");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
